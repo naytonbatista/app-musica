0cbeb24cb1ca221d9c658a95a5fee26afb48c153
jsiman <jamill.siman@gmail.com>
2018-05-18 17:30:41 -0400

    Alteração Api e Rotina de Inclusão de Usuário.

--- src/routes/usuario.php
+++ src/routes/usuario.php
@@ -16,20 +16,22 @@ $app->post('/api/usuario', function($request, $response, $args){
     $data = $request->getParsedBody();
 
     $pdo = $this->db->getPdo();
-
-    $stmt = $pdo->prepare('CALL INCLUIR_USUARIO(?, ?, ?, ?, ?, ?)');
-
-    $v_commit  = 'S';
-    $v_retorno = '';
-    $teste2 = null;
-
-    $stmt->bindParam(1, $teste2, PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000);
-    $stmt->bindParam(2, $data["P_USU_EMAIL"], PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000); 
-    $stmt->bindParam(3, $data["P_USU_SENHA"], PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000); 
-    $stmt->bindParam(4, $v_commit, PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000); 
-    $stmt->bindParam(5, $teste2, PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000); 
-    $stmt->bindParam(6, $v_retorno, PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000); 
-    $stmt->execute();
     
-    return $response->withJson($v_retorno);
+    //ESPECIFICA OS PARAMETROS DE ENTRADA E SAÍDA DO PROCEDIMENTO
+    $stmt = $pdo->prepare("CALL INCLUIR_USUARIO(@P_ID_USUARIO, :P_USU_EMAIL, :P_USU_SENHA, :P_COMMIT, @P_OK, @P_RETORNO)");
+   
+    // PARAMETROS DE ENTRADA
+    $stmt->bindParam(":P_USU_EMAIL", $data["P_USU_EMAIL"], PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000); 
+    $stmt->bindParam(":P_USU_SENHA", $data["P_USU_SENHA"], PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000); 
+    $stmt->bindParam(":P_COMMIT",    $data["P_COMMIT"],    PDO::PARAM_STR|PDO::PARAM_INPUT_OUTPUT, 4000); 
+
+    $stmt->execute();
+
+    //PARAMETRO DE SAÍDA    
+    $retorno["P_ID_USUARIO"] = $pdo->query("SELECT @P_ID_USUARIO")->fetch(PDO::FETCH_ASSOC);;
+    $retorno["P_OK"]         = $pdo->query("SELECT @P_OK")->fetch(PDO::FETCH_ASSOC);
+    $retorno["P_RETORNO"]    = $pdo->query("SELECT @P_RETORNO")->fetch(PDO::FETCH_ASSOC);;
+
+    return $response->withJson($retorno);
+
 });
